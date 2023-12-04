20086426458dabd1d9f9f3939c56eac34196a959
Carolyn Moneymaker <carolyn_moneymaker@comcast.com>
2023-12-04 14:30:02 -0500

    day 4 part 1 solution

---
+++ challenges/day4/part1.js
@@ -0,0 +1,42 @@
+let data = require('./data')
+
+const filterFalsey = (num) => {
+    return !!num
+}
+
+
+const reformatData = () => {
+    data = data.split('\n')
+    data = data.map((item) => {
+        let obj = item.split(':')
+        const key = obj[0].slice(obj[0].length - 4, obj[0].length).trim()
+        const nums = obj[1].split(' | ')
+        const winning = nums[0].split(' ').filter(filterFalsey)
+        const mine =  nums[1].split(' ').filter(filterFalsey)
+        return obj = { [key]: { winning, mine } }
+    })
+    return data
+}
+
+const reformattedData = reformatData()
+
+const findMatchesAndConvertToPoints = () => {
+    return reformattedData.map((object) => {
+        // console.log(Object.values(object)[0].winning)
+        // console.log(Object.values(object)[0].mine)
+        let count = 0
+        Object.values(object)[0].winning.forEach((val) => {
+            if (Object.values(object)[0].mine.includes(val)){
+                count === 0 ? count++ : count *= 2 // increment first match by 1 then double each subsequent match (exponential)
+            }
+        })
+        // console.log({ [Object.keys(object)[0]] : count })
+        return count
+    })
+}
+
+const getTotalPoints = () => {
+    return findMatchesAndConvertToPoints().reduce((a, b) => a + b)
+}
+
+module.exports = getTotalPoints
