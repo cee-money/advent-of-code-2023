32ffa5690328ec25ada1aeaf056f86c2df332d8e
Carolyn Moneymaker <carolyn_moneymaker@comcast.com>
2023-12-04 10:15:34 -0500

    set up day3

---
+++ challenges/day3/part1.js
@@ -0,0 +1,12 @@
+let data = require('./data')
+const mathjs = require('mathjs')
+
+data = data.split('\n')
+
+console.log(data)
+
+// TODO
+// Convert to matrices with mathjs?
+// https://mathjs.org/docs/datatypes/matrices.html
+// https://mathjs.org/examples/matrices.js.html
+// https://www.npmjs.com/package/mathjs
